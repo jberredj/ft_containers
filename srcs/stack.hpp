3e4d0f1ab57224e2cd4da858ce670a9aff47ade1
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2023-02-03 11:48:01 +0100

    stack: Operators

--- srcs/stack.hpp
+++ srcs/stack.hpp
@@ -22,9 +22,39 @@ namespace ft {
 		const_reference& top() const { return c.back(); }
 		void push(const value_type& value) { c.push_back(value); }
 		void pop(void) { c.push_back(value); }
+
+		stack& operator=(const stack& src) {
+			c =  src.c;
+			return *this;
+		}
 	protected:
 		container_type c;
 	}
 }
+
+template <class T, class Container>
+bool	operator==(const stack<T, Container>& lhs, 
+					const stack<T, Container>& rhs) { return lhs.c == rhs.c; }
+
+template <class T, class Container>
+bool	operator!=(const stack<T, Container>& lhs, 
+					const stack<T, Container>& rhs) { return !(lhs == rhs); }
+
+template <class T, class Container>
+bool	operator<(const stack<T, Container>& lhs, 
+					const stack<T, Container>& rhs) { return lhs.c < rhs.c; }
+
+template <class T, class Container>
+bool	operator<=(const stack<T, Container>& lhs, 
+					const stack<T, Container>& rhs) { return !(lhs < rhs); }
+
+template <class T, class Container>
+bool	operator>(const stack<T, Container>& lhs, 
+					const stack<T, Container>& rhs) { return rhs < lhs; }
+
+template <class T, class Container>
+bool	operator>=(const stack<T, Container>& lhs, 
+					const stack<T, Container>& rhs) { return !(rhs < lhs); }
+
 
 #endif
