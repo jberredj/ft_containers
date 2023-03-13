6930dc8fee759919019512d8016b3fc04c203e09
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2023-03-13 14:43:27 +0100

    add constructor and assignement function

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -19,21 +19,70 @@ namespace ft
 		typedef Allocator allocator_type;
 		typedef size_t size_type;
 		typedef ptrdiff_t difference_type;
-		typedef value_type& reference;
-		typedef const value_type& const_reference;
+		typedef value_type &reference;
+		typedef const value_type &const_reference;
 		typedef typename Allocator::pointer pointer;
 		typedef typename Allocator::const_pointer const_pointer;
 		typedef typename ft::random_access_iterator<value_type> iterator;
 		typedef typename ft::cRandom_access_iterator<value_type> const_iterator;
-		typedef typename ft:reverse_iterator<iterator> reverse_iterator;
-		typedef typename ft:reverse_iterator<const_iterator> const_reverse_iterator;
+		typedef typename ft : reverse_iterator<iterator> reverse_iterator;
+		typedef typename ft : reverse_iterator<const_iterator> const_reverse_iterator;
+
 	private:
-		allocator_type	_alloc;
-		pointer			_array;
-		size_type		_size;
-		size_type		_capacity;
+		allocator_type _alloc;
+		pointer _array;
+		size_type _size;
+		size_type _capacity;
+
 	public:
-		
+		// giving default value to allocator make this constructor the default one
+		explicit vector(const Allocator &alloc = allocator_type()) : _alloc(alloc), _array(NULL), _size(0), _capacity(0) {}
+		explicit vector(size_type count, const T &value = T(), const Allocator &alloc = Allocator())
+			: _alloc(alloc),
+			  _size(count), _capacity(count)
+		{
+			_array = _alloc.allocate(count);
+			for (size_type i = 0; i < _size; i++)
+				_alloc.construct(&_array[i], value);
+		}
+		template <class InputIt>
+		vector(InputIt first, InputIt last, const Allocator &alloc = Allocator()) : _alloc(alloc), _size(0), _capacity(0) // TODO: Some implemetation uses enable_if<is_integral> figure out why
+		{
+			assign(first, last); // TODO: implement assign
+		}
+
+		vector(const vector &other)
+		{
+			assign(other.begin(), other.end());
+		}
+
+		~vector()
+		{
+			clear();							  // TODO: implement clear;
+			_alloc.deallocate(_value, _capacity); // clear() will be in charge of destroy
+		}
+
+		vector &operator=(const vector &other)
+		{
+			clear();
+			assign(other.begin(), other.end());
+			return *this;
+		}
+
+		void assign(size_type count, const T &value)
+		{
+			clear();
+			insert(begin(), count, value); // TODO: implement insert
+		}
+
+		template <class InputIt>
+		void assign(InputIt first, InputIt last)
+		{
+			clear();
+			insert(begin(), first, last);
+		}
+
+		allocator_type get_allocator() const { return _alloc; }
 	};
 }
 
